8da73772a1b0235d26f6c1f14946c157967d3c55
Kojiro-schatten <banbirou@icloud.com>
2022-01-23 17:51:16 +0900

    add: random pokemon id fetcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { getOptionsForVote } from './utils/getRandomPokemon'
 import { trpc } from './utils/trpc'
-// import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const {data, isLoading} = trpc.useQuery(["hello", {text: "Kojir"}])
-  if(isLoading) return <div>Loading...</div>
-  if(data) return <div>{data.greeting}</div>
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-center-middle">
       <div className="text-2xl text-center">Which pokemon is Rounder</div>
       <div className="p-2 flex justify-center">
         <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-          <div className='w-16 h-16 bg-red-200'></div>
+          <div className='w-16 h-16 bg-red-800'>{first}</div>
           <div className='p-8'>VS</div>
-          <div className='w-16 h-16 bg-red-200'></div>
+          <div className='w-16 h-16 bg-red-800'>{second}</div>
         </div>
       </div>
     </div>
