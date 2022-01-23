bab6cf5a32a5090479f52cfe3691def04fcdacf4
Kojiro-schatten <banbirou@icloud.com>
2022-01-23 17:43:29 +0900

    add: tRPC

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { trpc } from './utils/trpc'
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const {data, isLoading} = trpc.useQuery(["hello", {text: "Kojir"}])
+  if(isLoading) return <div>Loading...</div>
+  if(data) return <div>{data.greeting}</div>
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-center-middle">
       <div className="text-2xl text-center">Which pokemon is Rounder</div>
