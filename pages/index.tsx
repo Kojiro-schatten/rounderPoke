89b8a84d9de4464173a4379a937f4db7509abcac
Kojiro-schatten <banbirou@icloud.com>
2022-01-23 15:37:05 +0900

    modify: style

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,12 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-center-middle">
       <div className="text-2xl text-center">Which pokemon is Rounder</div>
-      <div className="p-2"/>
-      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className='w-16 h-16 bg-red-200'></div>
-        <div className='p-8'>VS</div>
-        <div className='w-16 h-16 bg-red-200'></div>
+      <div className="p-2 flex justify-center">
+        <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+          <div className='w-16 h-16 bg-red-200'></div>
+          <div className='p-8'>VS</div>
+          <div className='w-16 h-16 bg-red-200'></div>
+        </div>
       </div>
     </div>
   )
