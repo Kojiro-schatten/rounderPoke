6eb92fa0e77a691f89b470ec4320f5955aa38d41
Kojiro-schatten <banbirou@icloud.com>
2022-01-23 15:00:54 +0900

    add: basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,15 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl red">hello</div>
+    <div className="h-screen w-screen flex flex-col justify-center align-center-middle">
+      <div className="text-2xl text-center">Which pokemon is Rounder</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='p-8'>VS</div>
+        <div className='w-16 h-16 bg-red-200'></div>
+      </div>
+    </div>
   )
 }
 
